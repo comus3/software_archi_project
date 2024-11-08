fcfbf30e710f755de4e01aca868d078912550b39
Oussama <195052@ecam.be>
2024-11-08 01:26:16 +0100

    Text "Welcome+Username"

---
+++ SchoolManager/composeApp/src/commonMain/kotlin/org/schoolmanager/project/HomePage.kt
@@ -0,0 +1,26 @@
+package org.schoolmanager.project
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.ui.text.style.TextDecoration
+
+
+
+class HomePage {
+    @Composable
+    fun Welcome(name: String) {
+        BasicText(
+            text= "Hello, $name!",
+            style= TextStyle(
+                color= Color.Black,
+                fontSize= 28.sp,
+                fontWeight= FontWeight.Bold,
+                textDecoration= TextDecoration.Underline
+            )
+        )
+    }
+}
