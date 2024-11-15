4aa3bbb1a396035fa06ac386991e4c073f884f68
LoricLannister <marcellusaba@gmail.com>
2024-11-15 13:44:15 +0100

    Logique MVVM faite

--- SchoolManager/composeApp/src/iosMain/kotlin/org/schoolmanager/project/MainViewController.kt
+++ SchoolManager/composeApp/src/iosMain/kotlin/org/schoolmanager/project/MainViewController.kt
@@ -1,5 +1,6 @@
 package org.schoolmanager.project
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.schoolmanager.project.ui.calendar.App
 
 fun MainViewController() = ComposeUIViewController { App() }
