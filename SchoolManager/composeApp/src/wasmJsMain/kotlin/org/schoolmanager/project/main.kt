860169338ada98f9356d07016aa133405ccbcf77
GaripA <garip.akkaya9@gmail.com>
2024-12-05 08:52:16 +0100

    correction dépréciations

--- SchoolManager/composeApp/src/wasmJsMain/kotlin/org/schoolmanager/project/main.kt
+++ SchoolManager/composeApp/src/wasmJsMain/kotlin/org/schoolmanager/project/main.kt
@@ -3,11 +3,7 @@ package org.schoolmanager.project
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
-import org.schoolmanager.project.ui.grades.App
-import org.schoolmanager.project.ui.profile.App
-import org.schoolmanager.project.ui.calendar.App
-import org.schoolmanager.project.ui.contacts.App
-import org.schoolmanager.project.ui.ui.homepage.App
+
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
